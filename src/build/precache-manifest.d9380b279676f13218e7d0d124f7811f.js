90dfaf0f4a03903c5f350a7aab9efdbd1f225a0b
Denis Kuivalainen <kuivalainen@gmail.com>
2019-11-23 21:51:44 +0200

    built1
    Вытащить папку build в корневой каталог

---
+++ src/build/precache-manifest.d9380b279676f13218e7d0d124f7811f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26656701aac2d616e69dbf59f289105b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51114c8707fa6f54ba54",
+    "url": "/static/css/main.26a74f79.chunk.css"
+  },
+  {
+    "revision": "250a7b11138e68f58481",
+    "url": "/static/js/2.048be9cf.chunk.js"
+  },
+  {
+    "revision": "51114c8707fa6f54ba54",
+    "url": "/static/js/main.4007520d.chunk.js"
+  },
+  {
+    "revision": "902c862e1443fd622c23",
+    "url": "/static/js/runtime-main.11af767d.js"
+  },
+  {
+    "revision": "aff1273104d60398579b149f6864a28b",
+    "url": "/static/media/logo.aff12731.jpg"
+  }
+]);
