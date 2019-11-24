bf2220c58f533e70dfb27010ddd5ca2f3a3e915d
Denis Kuivalainen <kuivalainen@gmail.com>
2019-11-24 22:18:42 +0200

    Prod f+b
    Полностью рабочая версия

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App1.css';
 import './App2.css';
 import logo from './logo.jpg';
-import testjson from './index.json'
 
 class App extends React.Component {
   constructor (props) {
@@ -13,9 +12,10 @@ class App extends React.Component {
       inp3: '',
       recp: '',
       name: '',
-      url: '',
+      url: "",
     }
     this.FromServ = this.FromServ.bind(this);
+    this.keydownHandler = this.keydownHandler.bind(this);
   }
 
   //Лого
@@ -38,7 +38,7 @@ class App extends React.Component {
   }
 
   //Выводим рецепт
-  Protocol11() {
+  field_recp() {
     if (this.state.recp !== '') {
       return(
         <div class="jumbotron jumbotron-fluid">
@@ -50,30 +50,33 @@ class App extends React.Component {
       )
     }
   }
-    
-  //Записываем рецепт и обнуляем ингридиенты
+
+  //Записываем API и обнуляем ингридиенты
   FromServ() {
-    let nurl = 'http://79.141.152.102/back.php' + this.state.inp1
+    let urlstr = "http://79.141.152.102/back.php?ing1=" + this.state.inp1 + "&ing2=" + this.state.inp2 + "&ing3=" + this.state.inp3;
     this.setState({ 
-      recp: "Loading...",
-      url : nurl,
-      inp1: '',
-      inp2: '',
-      inp3: '',
+      inp1 : '',
+      inp2 : '',
+      inp3 : '',
+      url : urlstr,
+      name : 'Loading...',
+      recp : 'TIP: You can use only 6 ingredients: potato, rice, tomatoes, cream, meat and paprika.',
     });
-    console.log(this.state.url)
   }
   
-  componentDidMount() {
-    const urll = "http://79.141.152.102/back.php"
-    fetch(urll).then(res => res.json()).then(json => {
-      console.log(json)
-      // this.setState({ 
-      //   recp: jsonData.Description,
-      //   name : jsonData.Name
-      // })
-    }).catch((error) => {console.error(error)})
-   
+  //Вывод рецепта
+  componentDidUpdate() {
+    if (this.state.url !== '') {
+      const urll = this.state.url
+      fetch(urll).then(res => res.json()).then(json => {
+        //console.log(json[0].Id);
+        this.setState({ 
+          recp: json[0].Description,
+          name : json[0].Name,
+          url : '',
+        });
+      }).catch((error) => {console.error(error)})  
+    }
   }
 
   //Выводим форму для ингридиента 2
@@ -115,15 +118,24 @@ class App extends React.Component {
   }
 
   // Активация по Enter
-  // keydownHandler(e){
-  //   if(e.keyCode===13) {this.FromServ()}
-  // }
-  // componentDidMount(){
-  //   document.addEventListener('keydown',this.keydownHandler);
-  // }
-  // componentWillUnmount(){
-  //   document.removeEventListener('keydown',this.keydownHandler);
-  // }
+  keydownHandler(e){
+    if(e.keyCode===13) {
+      let urlstr = "http://79.141.152.102/back.php?ing1=" + this.state.inp1 + "&ing2=" + this.state.inp2 + "&ing3=" + this.state.inp3;
+      this.setState({ 
+        inp1 : '',
+        inp2 : '',
+        inp3 : '',
+        url : urlstr,
+        recp : 'Loading...'
+      });
+    }
+  }
+  componentDidMount(){
+    document.addEventListener('keydown',this.keydownHandler);
+  }
+  componentWillUnmount(){
+    document.removeEventListener('keydown',this.keydownHandler);
+  }
 
   render() {
     return (
@@ -139,7 +151,7 @@ class App extends React.Component {
         {/* Рецепт */}
         <div class="row  "> 
           <div class="col-lg-12 col-12 ">
-            {this.Protocol11()}
+            {this.field_recp()}
           </div>
         </div>
 
@@ -148,7 +160,7 @@ class App extends React.Component {
 
           {/* Кнопка */}
           <div class="col-lg-3 col-12 hrow" >
-            <button class="btn btn-outline-secondary " type="button" onClick={this.FromServ}>Generate</button>
+            <button class="btn btn-outline-secondary " type="button" onClick={this.FromServ}>Search recipe</button>
           </div>
 
           {/* Поле ввода 1 */}
